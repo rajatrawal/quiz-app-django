29fbda3dfef08ca1e0059720ef6b3be390a15846
Rajat <rwlrajat@gmail.com>
2023-12-05 13:57:50 +0530

    initail commit

--- quiz/settings.py
+++ quiz/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = config('KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['']
+ALLOWED_HOSTS = ['quiz-app-django.onrender.com']
 
 
 # Application definition
